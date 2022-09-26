258ca50a97ceb2bee93d11cd3573c7ae3121158e
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-09-26 18:55:29 +0530

    [JS][Polyfill][Filter] example solution

--- polyfill/myFilter.js
+++ polyfill/myFilter.js
@@ -5,10 +5,19 @@ console.log(arr.filter(item => item > 5));
 Array.prototype.myFilter = function (callback) {
     const newArray = [];
     for (let i = 0; i < this.length; i++) {
-        const result = callback(this[i]);
+        const result = callback(this[i], i, this);
         if (result) newArray.push(this[i]);
     }
     return newArray;
 }
 
 console.log(arr.myFilter(item => item > 5));
+
+const fruits = ['apple', 'mango', 'pineapple', 'grapes', 'orange', 'banana'];
+
+const leftFruits = fruits.myFilter((fruit, index, arr) => {
+    arr.pop();
+    return fruit.length < 7;
+});
+
+console.log(leftFruits);
